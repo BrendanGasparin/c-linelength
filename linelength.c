5b95bddeda03dac52ba71ffb2eed5eab8150746d
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-04-21 19:01:17 +1000

    Initial commit

---
+++ linelength.c
@@ -0,0 +1,32 @@
+/*
+    linelength.c
+    Author: Brendan Gasparin <Brendan.Gasparin@gmail.com>
+    Date: 21 April 2014
+    Reads from the standard input and prints each line to the output,
+    along with its length.
+*/
+
+#include <stdio.h>
+
+#define MAXLINE 1000    // maximum input line size
+
+void copy(char to[], char from[]);
+int getline(char line[], int maxline);
+
+int main()
+{
+    int len;            // current line length
+    char line[MAXLINE]; // current input line
+
+    return 0;
+}
+
+void copy(char to[], char from[])
+{
+
+}
+
+int getline(char line[], int maxline)
+{
+    return 0;
+}
